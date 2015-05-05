cd31110e26945d1b0972ed88fa1462ed705d8b5a
ShinDarth <borzifrancesco@gmail.com>
2015-05-05 12:24:36 +0200

    Search results (TODO)

--- js/app/engine.js
+++ js/app/engine.js
@@ -3,7 +3,22 @@
 
   app.controller("HomeController", function($scope, $http) {
 
-    // TODO
+    var itemPanel = angular.element(document.querySelector("#item-panel"));
+    var npcPanel = angular.element(document.querySelector("#npc-panel"));
+    var questPanel = angular.element(document.querySelector("#quest-panel"));
+
+    $scope.search = function(searchstr) {
+
+      itemPanel.addClass("hidden");
+      npcPanel.addClass("hidden");
+      questPanel.addClass("hidden");
+
+      itemPanel.removeClass("hidden");
+      npcPanel.removeClass("hidden");
+      questPanel.removeClass("hidden");
+
+      // TODO
+    };
 
   });
 
