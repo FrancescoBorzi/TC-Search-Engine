bfb82472eb7936e6af1114d632ff0df01d3dd348
ShinDarth <borzifrancesco@gmail.com>
2015-05-05 15:38:28 +0200

    Basic search

--- js/app/engine.js
+++ js/app/engine.js
@@ -13,11 +13,54 @@
       npcPanel.addClass("hidden");
       questPanel.addClass("hidden");
 
-      itemPanel.removeClass("hidden");
-      npcPanel.removeClass("hidden");
-      questPanel.removeClass("hidden");
+      /* looking for items... */
+      var request = app.api + "item/template/" + searchstr;
 
-      // TODO
+      $http.get( request )
+      .success(function(data, status, header, config) {
+
+        $scope.items = data;
+        if ($scope.items.length > 0) { // we found items
+          itemPanel.removeClass("hidden");
+        }
+
+      })
+      .error(function(data, status, header, config) {
+        console.log("Error in $http.get request: " + request);
+      });
+
+      /* looking for NPCs... */
+      var request = app.api + "creature/template/" + searchstr;
+
+      $http.get( request )
+      .success(function(data, status, header, config) {
+
+        $scope.npcs = data;
+        if ($scope.npcs.length > 0) { // we found npcs
+          npcPanel.removeClass("hidden");
+        }
+
+      })
+      .error(function(data, status, header, config) {
+        console.log("Error in $http.get request: " + request);
+      });
+
+      /* looking for Quests... */
+      var request = app.api + "quest/template/" + searchstr;
+
+      $http.get( request )
+      .success(function(data, status, header, config) {
+
+        $scope.quests = data;
+        if ($scope.quests.length > 0) { // we found quests
+          questPanel.removeClass("hidden");
+        }
+
+      })
+      .error(function(data, status, header, config) {
+        console.log("Error in $http.get request: " + request);
+      });
+
     };
 
   });
