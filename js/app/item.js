686a19a7fa869b47483bfa61eb0ce6c37530633b
ShinDarth <borzifrancesco@gmail.com>
2015-05-07 00:06:45 +0200

    Show names of NPCs dropping the item

--- js/app/item.js
+++ js/app/item.js
@@ -4,6 +4,7 @@
 
   app.controller("ItemController", function($scope, $stateParams, $http) {
 
+    /* get item template */
     var request = app.api + "item/template/" + $stateParams.id;
 
     $http.get( request )
@@ -18,6 +19,7 @@
       console.log("Error in ITEM TEMPLATE $http.get request");
     });
 
+    /* get npc vendors */
     request = app.api + "vendor/item/" + $stateParams.id;
 
     $http.get( request )
@@ -33,6 +35,7 @@
       console.log("Error in NPC VENDOR $http.get request");
     });
 
+    /* get creature loot template */
     request = app.api + "loot/creature/item/" + $stateParams.id;
 
     $http.get( request )
@@ -48,6 +51,7 @@
       console.log("Error in CREATURE LOOT $http.get request");
     });
 
+    /* get skinning loot template */
     request = app.api + "loot/skinning/item/" + $stateParams.id;
 
     $http.get( request )
@@ -63,6 +67,7 @@
       console.log("Error in SKINNING LOOT $http.get request");
     });
 
+    /* get gameobject loot template */
     request = app.api + "loot/gameobject/item/" + $stateParams.id;
 
     $http.get( request )
